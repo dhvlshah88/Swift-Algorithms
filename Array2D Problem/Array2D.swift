4a69b3f01cccb858bb8af4e151c8f83ba597903c
Dhaval Shah <dhvlshah88@gmail.com>
2016-08-31 18:28:56 -0700

    Added auxiliary code file to Array2D playground to separate code in its own class file. Also included that class file.

---
+++ Array2D Problem/Array2D.swift
@@ -0,0 +1,50 @@
+import Foundation
+
+
+public class Array2D : NSObject {
+    
+    public override init() {
+        
+    }
+    
+    public func printSpiral(matrix: [[Int]], m: Int, n: Int) -> Void {
+        var startRow = 0
+        var endRow = m
+        var startCol = 0
+        var endCol = n
+        
+        var arr : [Int] = []
+        arr.reserveCapacity(m*n)
+        
+        while startRow < endRow && startCol < endCol {
+            
+            for i in startCol ..< endCol {
+                arr.append(matrix[startRow][i])
+            }
+            startRow += 1;
+            
+            for i in startRow ..< endRow {
+                arr.append(matrix[i][endRow-1])
+            }
+            endCol -= 1;
+            
+            if startRow < endRow {
+                for i in (endCol-1).stride(through: startCol, by: -1) {
+                    arr.append(matrix[endRow-1][i])
+                }
+                endRow -= 1
+            }
+            
+            if startCol < endCol {
+                for i in (endRow-1).stride(through: startRow, by: -1) {
+                    arr.append(matrix[i][startCol])
+                }
+                startCol += 1
+            }
+        }
+        
+        print(arr)
+    }
+}
+
+
